20586adea78a80afb6e06bcfd0e3d0d2b8129851
Alex Olivier <aolivier34@gmail.com>
2024-12-10 04:36:23 -0800

    Update service_record_manager.py

--- chocan_software/data_managers/service_record_manager.py
+++ chocan_software/data_managers/service_record_manager.py
@@ -20,7 +20,6 @@ class ServiceRecordManager:
             member = session.query(Member).filter_by(id=member_id).first()
             service = session.query(Service).filter_by(id=service_id).first()
             if not provider or not member or not service:
-                print("\nInvalid provider, member, or service code.")
                 return
 
             new_service_record = ServiceRecord(
@@ -32,7 +31,6 @@ class ServiceRecordManager:
             )
             session.add(new_service_record)
             session.commit()
-            print(f"\n>> Service recorded for {member.name} by {provider.name}")
     
     def update_service_record(self, record_id, **kwargs):
         record_id = int(record_id)
@@ -68,45 +66,28 @@ class ServiceRecordManager:
                 for record in records:
                     print(f"  {record.id:06}: {record.service_date} - {record.member.name:<25} - {record.service.name}")
     
-    def is_valid_service_record(self, record_id):
-        record_id = int(record_id)
-        with self.db_manager.get_session() as session:
-            record = session.query(ServiceRecord).filter_by(id=record_id).first()
-            return record if record else None
-
     def get_service_record(self, record_id):
         record_id = int(record_id)
         with self.db_manager.get_session() as session:
             record = session.query(ServiceRecord).filter_by(id=record_id).first()
-            return record if record else None
+            return record
     
     def get_service_records_by_provider(self, provider_number):
         provider_id = int(provider_number)
         with self.db_manager.get_session() as session:
             records = session.query(ServiceRecord).filter_by(provider_id=provider_id).all()
-            return records if records else None
+            return records
     
     def get_service_records_by_member(self, member_number):
         member_id = int(member_number)
         with self.db_manager.get_session() as session:
             records = session.query(ServiceRecord).filter_by(member_id=member_id).all()
-            return records if records else None
+            return records
 
-    # Uses strings for the start_ddate and end_date
-    def get_service_records_by_date_range(self, start_date: str, end_date: str):
-        start_date = datetime.strptime(start_date, "%m-%d-%Y")
-        end_date = datetime.strptime(end_date, "%m-%d-%Y")
-        with self.db_manager.get_session() as session:
-            records = session.query(ServiceRecord).filter(
-                ServiceRecord.service_date.between(start_date, end_date)
-            ).all()
-            return records if records else None
-    
-    # uses datetime objects instead of strings
     def query_service_records_by_date_range(self, start_date: datetime, end_date: datetime):
         with self.db_manager.get_session() as session:
             records = session.query(ServiceRecord).filter(
                 ServiceRecord.service_date >= start_date,
                 ServiceRecord.service_date <= end_date
             ).all()
-            return records if records else None
+            return records
